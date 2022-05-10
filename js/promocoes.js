377bc0771af77edd9ebb78541fd2060768f05f36
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-05-10 14:29:49 -0300

    começo antes da reuniao

--- js/promocoes.js
+++ js/promocoes.js
@@ -1,26 +1,65 @@
 console.log("testando Js")
 
 var filmes = [
-    ["A Freira", "afreira.jpeg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!"],
-    ["A hora Do Rush", "bateroucorrer.jpeg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!"],
-    ["Corra", "corra.jpeg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!"],
-    ["Em Defesa de Cristo", "cristo.jpeg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!"],
-    ["Homem Aranha: Sem volta para Casa", "semvoltapracasa.jpeg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!"],
-    ["Shrek", "sherek.jpeg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!"],
-    ["Tô Ryca", "toryca.jpeg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!"],
-    ["Veloses e Furiosos", "velosesefuriosos.jpeg", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!"]
+    ["A Freira",
+        "afreira.jpeg",
+        "[CATEGORIA]",
+        "[ANO LANÇAMENTO]",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!"
+    ],
+
+    ["A hora Do Rush",
+        "bateroucorrer.jpeg",
+        "[CATEGORIA]",
+        "[ANO LANÇAMENTO]",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!"
+    ],
+
+    ["Corra",
+        "corra.jpeg",
+        "[CATEGORIA]",
+        "[ANO LANÇAMENTO]",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!",
+    ],
+
+    ["Em Defesa de Cristo",
+        "cristo.jpeg",
+        "[CATEGORIA]",
+        "[ANO LANÇAMENTO]",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!",
+    ],
+
+    ["Homem Aranha: Sem volta para Casa",
+        "semvoltapracasa.jpeg",
+        "[CATEGORIA]",
+        "[ANO LANÇAMENTO]",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!",
+    ],
+
+    ["Shrek",
+        "sherek.jpeg",
+        "[CATEGORIA]",
+        "[ANO LANÇAMENTO]",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae",
+
+    ],
+
+    ["Tô Ryca",
+        "toryca.jpeg",
+        "[CATEGORIA]",
+        "[ANO LANÇAMENTO]",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!",
+    ],
+
+    ["Veloses e Furiosos",
+        "velosesefuriosos.jpeg",
+        "[CATEGORIA]",
+        "[ANO LANÇAMENTO]",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore excepturi necessitatibus, iure iusto, odio ullam nihil id maxime reprehenderit sequi pariatur perspiciatis. Aspernatur blanditiis voluptatum provident labore natus repudiandae!",
+
+    ]
 ]
+
 console.log(filmes)
-
-var galeria = document.querySelector("#galeria")
-
-for (var i = 0; i < filmes.length; i++) {
-    var img = new Image()
-    img.src = "imagens/" + filmes[i][1]
-    img.width = 120
-    galeria.innerHTML += "<h1>" + "<strong>" + filmes[i][0] + "<br>"
-    galeria.appendChild(img)
-    galeria
-    galeria.innerHTML += "<p>" + filmes[i][2] + "<hr>"
-
-}
+// Galeria Faz acesso ao elemento div com id=Galeria
+let galeria = document.querySelector('#galeria')
