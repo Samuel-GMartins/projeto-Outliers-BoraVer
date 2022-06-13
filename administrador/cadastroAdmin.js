21fed2caebcc7af83e6d1370843597b808d2975d
TechComLari <lary.mc12@hotmail.com>
2022-06-13 16:33:06 -0300

    att

--- administrador/cadastroAdmin.js
+++ administrador/cadastroAdmin.js
@@ -1,9 +1,7 @@
-console.log(document.querySelector("h1"))
-
-document.querySelector("h1").innerHTML+=""
-
-console.log(document.forms.formCadastro)
 var formC=document.forms.formCadastro
+let verSenha = document.querySelector('#verSenha')
+let verConfSenha = document.querySelector('#verConfirmeSenha')
+
 //Evento de clique no botão confirmar
 document.querySelector("#btCadastro").onclick=function(){
     if(formC.nomeCadastro.value == ""){
@@ -14,18 +12,38 @@ document.querySelector("#btCadastro").onclick=function(){
             alert("Preencha a senha!")
     }else if(formC.senhaC.value == ""){
                 alert("Confirme a senha!")
-            }else if(formC.senhaC.value !== formC.senha.value){
+    }else if(formC.senhaC.value !== formC.senha.value){
                 alert("A senha não confere!")
-     }else if(formC.adms.value == ""){
-            alert("Você é Adm ou Adm Master?")
     }else{
         formC.submit()
+        
+        //Salvando dados em localStorage
         localStorage.emailUser=formC.emailCadastro.value 
         localStorage.nomeUser=formC.nomeCadastro.value
         localStorage.telefoneUser=formC.telefoneCadastro.value
         localStorage.senhaUser=formC.senha.value
         localStorage.qualAdm=formC.adms.value
-
     }
-
 }
+
+ // Evento de clique olhinho
+ verSenha.addEventListener('click', ()=>{
+    let inputSenha = document.querySelector('#senha')
+
+    if(inputSenha.getAttribute('type') == 'password'){
+        inputSenha.setAttribute('type', 'text')
+    } else {
+        inputSenha.setAttribute('type' , 'password')
+    }
+})
+
+ // Evento de clique olhinho confirme senha.
+verConfSenha.addEventListener('click', ()=>{
+    let inputConfirmeSenha = document.querySelector('#senhaC')
+
+    if(inputConfirmeSenha.getAttribute('type') == 'password'){
+        inputConfirmeSenha.setAttribute('type', 'text')
+    } else {
+        inputConfirmeSenha.setAttribute('type' , 'password')
+    }
+})
