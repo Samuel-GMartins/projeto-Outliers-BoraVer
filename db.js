ea53a374068ebe626c3f0213eba61ea52ccb865f
victormalve <victor.marques.alves@accenture.com>
2022-06-15 17:33:03 -0300

    alteração victo quarta feira

--- db.js
+++ db.js
@@ -63,9 +63,9 @@ async function cadastroAdmin(usuario){
 
 async function insertFilmes(filmes){
     const conectado = await conecta() 
-    const values = [filmes.titulo,filmes.genero,filmes.ano,filmes.sinopse,filmes.fotos,filmes.preco,filmes.promo]
+    const values = [filmes.titulo,filmes.genero,filmes.ano,filmes.sinopse,filmes.fotos,filmes.preco,filmes.promo,filmes.trailer]
     const [rows] = 
-    await conectado.query("INSERT INTO filmes(titulo,genero,ano,sinopse,fotos,preco,promo) VALUES (?,?,?,?,?,?,0)",values)      
+    await conectado.query("INSERT INTO filmes(titulo,genero,ano,sinopse,fotos,preco,promo,trailer) VALUES (?,?,?,?,?,?,0,?)",values)      
     return rows
 }
 
